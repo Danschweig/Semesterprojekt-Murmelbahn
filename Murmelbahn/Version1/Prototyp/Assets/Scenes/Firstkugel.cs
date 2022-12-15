64f818ea8b9d7804fe2c30f53035e9aed49e08e4
bene3576 <75899572+bene3576@users.noreply.github.com>
2022-12-15 18:43:23 +0100

    SwitchCam
    Kamera1 und 2 positioniert und Wechsel sobald eine kugel zu niedrig ist

--- Murmelbahn/Version1/Prototyp/Assets/Scenes/Firstkugel.cs
+++ Murmelbahn/Version1/Prototyp/Assets/Scenes/Firstkugel.cs
@@ -73,12 +73,7 @@ public class Firstkugel : MonoBehaviour
         else
             return GameObject.FindGameObjectWithTag("green4").transform.position;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
@@ -96,19 +91,6 @@ public class Firstkugel : MonoBehaviour
 
 
 
-        /*
-        if (GameObject.FindGameObjectWithTag("red").transform.position.y < GameObject.FindGameObjectWithTag("blue").transform.position.y
-                && GameObject.FindGameObjectWithTag("red").transform.position.y < GameObject.FindGameObjectWithTag("green").transform.position.y)
-        {
-            transform.position = GameObject.FindGameObjectWithTag("red").transform.position;
-        }
-        else if (GameObject.FindGameObjectWithTag("green").transform.position.y < GameObject.FindGameObjectWithTag("blue").transform.position.y
-                && GameObject.FindGameObjectWithTag("green").transform.position.y < GameObject.FindGameObjectWithTag("red").transform.position.y)
-        {
-            transform.position = GameObject.FindGameObjectWithTag("green").transform.position;
-        }
-        else
-            transform.position = GameObject.FindGameObjectWithTag("blue").transform.position;
-    */
+    
     }
 }
